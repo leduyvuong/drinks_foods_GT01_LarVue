cefd3dbe99153b3ba47f434590246448320a21c4
Le Duy Vuong <leduyvuong99@gmail.com>
2021-10-07 20:18:57 +0700

    view cart

--- fooddrink/src/App.tsx
+++ fooddrink/src/App.tsx
@@ -3,11 +3,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import ViewLogin from "./components/ViewLogin/ViewLogin";
 import ProductDetail from "./components/Single-product/ProductDetail";
+import Cart from "./components/cart_view/Cart";
 
 function App() {
   return (
     <div className="App">
-      <ProductDetail />
+      <Cart />
     </div>
   );
 }
