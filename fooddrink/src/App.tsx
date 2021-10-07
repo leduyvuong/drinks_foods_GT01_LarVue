d75b97fac03a60705e0b635da30394da0f560058
Le Duy Vuong <leduyvuong99@gmail.com>
2021-10-07 20:01:33 +0700

    view product detail

--- fooddrink/src/App.tsx
+++ fooddrink/src/App.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ViewLogin from "./components/ViewLogin/ViewLogin";
+import ProductDetail from "./components/Single-product/ProductDetail";
 
 function App() {
   return (
     <div className="App">
-      <ViewLogin />
+      <ProductDetail />
     </div>
   );
 }
